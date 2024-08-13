d8fdb9d916d76439758b345e78bd10a8f0e78f49
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-08-13 14:38:44 +0900

    シンボル型・コード重複修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,6 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
-      @week_days.push(days)
     end
 
   end
